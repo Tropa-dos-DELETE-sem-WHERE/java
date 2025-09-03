2482e580f1b3d4144d967ab136b31a7556ed4bfe
Mateus Silva <mateus.ssouza@sptech.school>
2025-09-03 19:53:12 -0300

    feat: consoles aleatórios

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,25 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.concurrent.ThreadLocalRandom;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        int logAleatorio = ThreadLocalRandom.current().nextInt(1,5);
+        String[] logs = new String[]
+                {
+                        "[DEBUG] SDADADADS",
+                        "[WARNING] SDADADADS",
+                        "[BD] SDADADADS",
+                        "[INFO] SDADADADS"
+                };
         try{
             consoles("Aplicação Iniciando: ");
             Thread.sleep(1000);
             for (int i = 0; i < 5; i++) {
-                consoles("Log da aplicação: ");
+                logAleatorio = ThreadLocalRandom.current().nextInt(1,5);
+                consoles(logs[logAleatorio-1]);
                 Thread.sleep(1000);
             }
             consoles("Aplicação Encerrando: ");
